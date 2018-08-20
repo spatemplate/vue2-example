539356f5f3eb3f15f8ae5d624a81794d56048b1c
Vitaliy <theyamshikov@yandex.ru>
2018-08-20 20:54:14 +0600

    include bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import router from './helpers/Router'
 import Layout from './components/Layout'
 import AutoRun from "./config/autorun";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-theme.min.css";
 
 new Vue({
     el: '#app',
