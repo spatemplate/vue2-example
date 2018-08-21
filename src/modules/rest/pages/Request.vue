cab0fec333af39e61363aff656d8b0a7a6ab6908
Vitaliy <theyamshikov@yandex.ru>
2018-08-21 23:54:03 +0600

    upgrade

--- src/modules/rest/pages/Request.vue
+++ src/modules/rest/pages/Request.vue
@@ -5,7 +5,6 @@
                 <div class="col-sm-2">
 
                     <div class="form-group field-requestform-method required">
-                        <label class="control-label sr-only" for="requestform-method">Method</label>
 
                         <select v-model="request.method" class="form-control" name="method" aria-required="true">
                             <option v-for="item in method.options" :value="item.value">
@@ -19,10 +18,10 @@
                 <div class="col-sm-10">
 
                     <div class="form-group field-requestform-endpoint">
-                        <label class="control-label sr-only" for="requestform-endpoint">endpoint</label>
                         <div class="input-group">
                             <div class="input-group-addon">{{ request.baseUrl }}/</div>
-                            <input v-model="request.uri" class="form-control" placeholder="endpoint" autofocus="" type="text">
+                            <input v-model="request.uri" class="form-control" placeholder="endpoint" autofocus=""
+                                   type="text">
                             <span class="input-group-btn">
                                 <button class="btn btn-primary" type="submit" tabindex="-1">Send</button>
                             </span>
@@ -31,15 +30,72 @@
                         <p class="help-block help-block-error"></p>
                     </div>
                 </div>
+                <div class="col-sm-12">
+                    <h4>Request</h4>
+                    <tabs>
+                        <tab title="Query">
+                            Query
+                        </tab>
+                        <tab title="Body">
+                            <br/>
+                            <table class="table">
+                                <tbody>
+
+                                <tr v-for="(value,key) in request.data">
+
+                                    <td class="column-check">
+                                        <div class="form-group field-requestform-bodyactives-1">
+                                            <input value="0" type="hidden">
+                                            <input value="1" checked="" tabindex="-1" type="checkbox">
+                                        </div>
+                                    </td>
+                                    <td class="column-key">
+                                        <div class="form-group form-group-sm field-requestform-bodykeys-1">
+                                            <input class="form-control" v-model="key" placeholder="Body Param" type="text">
+                                        </div>
+                                    </td>
+                                    <td class="column-value">
+                                        <div class="form-group form-group-sm field-requestform-bodyvalues-1">
+                                            <input class="form-control" v-model="request.data[key]" placeholder="Value" type="text">
+                                        </div>
+                                    </td>
+                                    <td class="column-actions">
+                                        <button type="button" class="close" tabindex="-1">
+                                            <span>×</span>
+                                        </button>
+                                    </td>
+
+                                </tr>
+
+                                </tbody>
+                            </table>
+                        </tab>
+                        <tab title="Headers">
+                            Headers
+                        </tab>
+                    </tabs>
+                </div>
             </div>
         </form>
 
-        <div v-if="response.data">
-            <pre>
-                <code id="response-content" class="json hljs">
-                    {{ response.data }}
-                </code>
-            </pre>
+        <div v-if="response">
+            <h4>Response</h4>
+            <tabs>
+                <tab title="Body">
+                    <br/>
+                    <pre><code class="json hljs">{{ JSON.stringify(response.data, null, 2) }}</code></pre>
+
+                </tab>
+                <tab title="Headers">
+                    <br/>
+                    <pre><code class="json hljs">{{ JSON.stringify(response.headers, null, 2) }}</code></pre>
+                </tab>
+
+                <span slot="nav-right">
+                    Status: {{ response.status }}
+                </span>
+
+            </tabs>
         </div>
 
     </div>
@@ -52,41 +108,43 @@
     export default {
         data() {
             return {
-                login: '',
-                password: '',
-                response: {
-                    data: null,
-                },
+                response: null,
                 request: {
                     baseUrl: store.config.server.domain,
                     method: 'get',
                     uri: 'v1/city',
+                    data: {
+                        login: 'admin',
+                        password: 'qwerty',
+                    },
                 },
                 method: {
                     selected: [],
                     options: [
-                        {value: 'get', label:'GET'},
-                        {value: 'post', label:'POST'},
-                        {value: 'put', label:'PUT'},
-                        {value: 'delete', label:'DELETE'},
+                        {value: 'get', label: 'GET'},
+                        {value: 'post', label: 'POST'},
+                        {value: 'put', label: 'PUT'},
+                        {value: 'delete', label: 'DELETE'},
                         //{value: 'options', label:'OPTIONS'},
                     ]
                 },
             }
         },
         methods: {
-            cb(response) {
-                this.response.data = JSON.stringify(response, null, 2);
-                //console.log(response);
+            setResponse(response) {
+                this.response = response;
+                console.log(response.headers);
             },
             send() {
-              // console.log(this.request.uri);
-               /* const cb = function (response) {
-                    this.response = JSON.stringify(response, null, 2);
-                    //console.log(response);
-                };*/
-                Rest.send(this.request, this.cb);
+                Rest.send(this.request, this.setResponse);
             }
         }
     }
 </script>
+
+<style>
+    code {
+        font-size: 10px;
+        color: red;
+    }
+</style>
